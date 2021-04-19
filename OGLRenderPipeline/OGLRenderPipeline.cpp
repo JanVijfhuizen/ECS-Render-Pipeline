e915f3f5d9ce3803ad1c1f69cd575af2b86ca565
Bonniebon <jan.vijfhuizen@outlook.com>
2021-04-19 16:51:31 +0200

    Project dump.
    This project currently contains cecsar, and glfw.
    It opens a window that freezes, which is the perfect start for a new project.

---
+++ OGLRenderPipeline/OGLRenderPipeline.cpp
@@ -0,0 +1,17 @@
+#include "Cecsar.h"
+#include <glfw3.h>
+
+int main()
+{
+	jecs::Cecsar cecsar;
+
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	while(true)
+	{
+		
+	}
+
+	return 0;
+}
