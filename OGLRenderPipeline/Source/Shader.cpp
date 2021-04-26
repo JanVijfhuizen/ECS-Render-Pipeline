bcd7f44bb598db3a70c9cd582da260d127fdf724
Jan Vijfhuizen <jan.vijfhuizen@outlook.com>
2021-04-26 11:09:48 +0200

    Added better shader class.

---
+++ OGLRenderPipeline/Source/Shader.cpp
@@ -0,0 +1,49 @@
+﻿#include "Shaders/Shader.h"
+
+#include "glm/ext.hpp"
+
+namespace rpi
+{
+	Shader::Shader(const GLuint program) : _program(program)
+	{
+		glUseProgram(_program);
+		_view = GetUniformLoc("view");
+		_projection = GetUniformLoc("projection");
+	}
+
+	Shader::~Shader()
+	{
+		glDeleteProgram(_program);
+	}
+
+	void Shader::Use(glm::vec3 eye, const glm::mat4& view, const glm::mat4& projection)
+	{
+		glUseProgram(_program);
+
+		// Forward view.
+		glUniformMatrix4fv(_view, 1, GL_FALSE, value_ptr(view));
+
+		// Forward projection.
+		glUniformMatrix4fv(_projection, 1, GL_FALSE, value_ptr(projection));
+	}
+
+	GLuint Shader::GetProgram() const
+	{
+		return _program;
+	}
+
+	GLuint Shader::GetView() const
+	{
+		return _view;
+	}
+
+	GLuint Shader::GetProjection() const
+	{
+		return _projection;
+	}
+
+	GLuint Shader::GetUniformLoc(const std::string& name) const
+	{
+		return glGetUniformLocation(_program, name.c_str());
+	}
+}
