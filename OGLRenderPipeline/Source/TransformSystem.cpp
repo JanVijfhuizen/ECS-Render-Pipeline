d7620337d60ce577f08387e68cf5b52f887c80ba
Jan Vijfhuizen <jan.vijfhuizen@outlook.com>
2021-04-29 19:51:12 +0200

    Optimized conversion between transform component and glm::mat4

--- OGLRenderPipeline/Source/TransformSystem.cpp
+++ OGLRenderPipeline/Source/TransformSystem.cpp
@@ -7,17 +7,46 @@ namespace rpi
 {
 	glm::mat4 TransformSystem::GetMatrix(const Transform& transform)
 	{
+		// Doing things manually instead of using glm is way faster for some reason.
+		// And it's not even close??
+
+		// Normally I wouldn't really care about this, because using glm is nice and easy.
+		// But this is such a massive bottleneck... the actual rendering doesn't even come close.
+		
+		// Translate.
 		glm::mat4 mat{ 1 };
-		mat = translate(mat, transform.position);
+		mat[3] = glm::vec4(transform.position, 1);
 
 		const auto& angles = transform.rotation;
-		const auto euler = glm::eulerAngleXYZ(
-			glm::radians(angles.x) / 2,
-			glm::radians(angles.y) / 2,
-			glm::radians(angles.z) / 2);
-		mat = mat * euler;
 
-		return scale(mat, transform.scale);
+		// Rotate X axis.
+		const float x = glm::radians(angles.x);
+		mat[1][1] = cos(x);
+		mat[2][1] = -sin(x);
+		mat[1][2] = sin(x);
+		mat[2][2] = cos(x);
+
+		// Rotate Y axis.
+		const float y = glm::radians(angles.y);
+		mat[0][0] = cos(y);
+		mat[2][0] = sin(y);
+		mat[2][2] = -sin(y);
+		mat[2][2] = cos(y);
+
+		// Rotate Z axis.
+		const float z = glm::radians(angles.z);
+		mat[0][0] = cos(z);
+		mat[1][0] = -sin(z);
+		mat[0][1] = sin(z);
+		mat[1][1] = cos(z);
+		
+		// Scale.
+		const auto& s = transform.scale;
+		mat[1][1] *= s.x;
+		mat[2][2] *= s.y;
+		mat[3][3] *= s.z;
+		
+		return mat;
 	}
 
 	glm::vec3 TransformSystem::GetForward(const Transform& transform)
